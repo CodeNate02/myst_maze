8b6767a02e40a148dedb300fe2fd071b8cebc5fa
CodeNate02 <n8opot8oW@gmail.com>
2022-10-26 09:45:56 -0400

    Load and draw spells

--- js/Myst_Maze.js
+++ js/Myst_Maze.js
@@ -1,3 +1,5 @@
+//Call the init function as soon as the page has finished loading
+window.onload = init;
 // Set Up Variables
 var canvas, context;
 const WIDTH = 600;
@@ -6,7 +8,7 @@ const event = new Event('playerMoved');
 const maze = {
 	screen: '../images/Game_Screen_v4.png',
 	start: { x: 515, y: 311 },
-	victory: { x: 98, y: 34 },
+	victory: { x: 96, y: 34 },
 	traps: {
 		fire: [
 			{ x: 134, y: 291 },
@@ -31,9 +33,27 @@ const maze = {
 		],
 	},
 	spells: {
-		water: [],
-		tp: [],
-		fire: [],
+		water: [
+			{ x: 474, y: 170 },
+			{ x: 394, y: 251 },
+			{ x: 354, y: 271 },
+			{ x: 95, y: 252 },
+			{ x: 215, y: 231 },
+		],
+		tp: [
+			{ x: 314, y: 151 },
+			{ x: 513, y: 32 },
+			{ x: 454, y: 251 },
+			{ x: 114, y: 271 },
+			{ x: 134, y: 171 },
+		],
+		fire: [
+			{ x: 254, y: 131 },
+			{ x: 114, y: 310 },
+			{ x: 514, y: 191 },
+			{ x: 454, y: 51 },
+			{ x: 334, y: 191 },
+		],
 	},
 };
 //Load image assets
@@ -61,9 +81,22 @@ class Sprite {
 		this.y = loc.y;
 	}
 }
-class PlayerSprite extends Sprite {
+class Player extends Sprite {
+	spriteState = 1;
+	movementStates = {
+		up: undefined,
+		down:undefined,
+		left:undefined,
+		right:undefined
+	}
 	constructor() {
 		super(maze.start);
+		window.addEventListener('keydown', ()=>{
+
+		});
+		window.addEventListener('keyup',()=>{
+
+		});
 	}
 }
 class TrapSprite extends Sprite {
@@ -83,20 +116,20 @@ class TrapSprite extends Sprite {
 		});
 	}
 	spell() {
-		if(spells[wins[this.type]]>0){
-            spells[wins[this.type]]-=1;
-            this.isVisible=false;
-            draw();
-        }
+		if (spells[wins[this.type]] > 0) {
+			spells[wins[this.type]] -= 1;
+			this.isVisible = false;
+			draw();
+		}
 	}
 }
 class SpellSprite extends Sprite {
-    type=undefined;
-    constructor(loc, type) {
-        super(loc);
-        this.type=type;
-        window.addEventListener(playerMoved,)
-    }
+	type = undefined;
+	constructor(loc, type) {
+		super(loc);
+		this.type = type;
+		window.addEventListener(playerMoved);
+	}
 }
 
 var cursor = {
@@ -107,18 +140,17 @@ var cursor = {
 	isVisible: true,
 };
 var wins = {
-    'pit':'teleport',
-    'spike':'fire',
-    'fire':'water'
-}
+	pit: 'teleport',
+	spike: 'fire',
+	fire: 'water',
+};
 var spells = {
-    teleport:1,
-    fire:0,
-    water:0
-}
+	teleport: 1,
+	fire: 0,
+	water: 0,
+};
 
-
-var player = new PlayerSprite();
+var player = new Sprite(maze.start);
 var score = 0;
 
 //Keyboard Control Variables
@@ -161,7 +193,6 @@ function init() {
 
 // Set Up Functions
 function game() {
-	//Generate victory element
 	//Generate Fire Traps
 	for (let f = 0; f < maze.traps.fire.length; f++) {
 		firetraps.push(new TrapSprite(maze.traps.fire[f], 'fire'));
@@ -175,23 +206,18 @@ function game() {
 		pitfalls.push(new TrapSprite(maze.traps.pit[f], 'pit'));
 	}
 	// //Generate fire spells
-	// for (let fs = 0; fs < 6; fs++) {
-	// 	firespells.push(new Sprite());
-	// 	firespells[fs].width = 10;
-	// 	firespells[fs].height = 18;
-	// }
+	for (let f = 0; f < maze.spells.fire.length; f++) {
+		firespells.push(new TrapSprite(maze.spells.fire[f], 'fire'));
+	}
 	// //Generate teleport spells
-	// for (let ts = 0; ts < 6; ts++) {
-	// 	telespells.push(new Sprite());
-	// 	telespells[ts].width = 15;
-	// 	telespells[ts].height = 15;
-	// }
+	for (let f = 0; f < maze.spells.tp.length; f++) {
+		telespells.push(new TrapSprite(maze.spells.tp[f], 'teleport'));
+	}
 	// //Generate water spells
 	// for (let ws = 0; ws < 6; ws++) {
-	// 	waterspells.push(new Sprite());
-	// 	waterspells[ws].width = 20;
-	// 	waterspells[ws].height = 20;
-	// }
+	for (let f = 0; f < maze.spells.water.length; f++) {
+		waterspells.push(new TrapSprite(maze.spells.water[f], 'water'));
+	}
 	//Listen and handle keypresses
 	window.addEventListener('keydown', handleKeyDown, true);
 	window.addEventListener('keyup', handleKeyUp, true);
@@ -205,16 +231,16 @@ function draw() {
 	//Clear canvas of shapes
 	//Potatoes (why did I comment 'Potatoes' here?  This was a sophomore-year me comment.  Present me has no idea, but I can't bring myself to delete it.  Long live the Potatoes.)
 	clear();
-	//Draw splashscreen
 	context.drawImage(backgroundImage, 0, 0);
 	//Draw player
 	context.drawImage(playerImage, player.x, player.y);
+	context.drawImage(cursorImage, cursor.x, cursor.y);
 	//Draw Victory Pickup
-	context.drawImage(Victory, victory.x, victory.y);
+	context.drawImage(Victory, maze.victory.x, maze.victory.y);
+
 	//Draw Fireball
 	pitfalls.forEach(pitfall => {
-		if (pitfall.isVisible)
-			context.drawImage(PitFall, pitfall.x, pitfall.y);
+		if (pitfall.isVisible) context.drawImage(PitFall, pitfall.x, pitfall.y);
 	});
 	//Draw Spike Trap
 	spiketraps.forEach(spiketrap => {
@@ -227,134 +253,20 @@ function draw() {
 			context.drawImage(FireTrap, firetrap.x, firetrap.y);
 	});
 	// //Draw Water Spell
-	// for (var ws = 0; ws < 6; ws++) {
-	// 	var waterspell = waterspells[ws];
-
-	// 	if (ws == 1) {
-	// 		waterspell.x = 474;
-	// 		waterspell.y = 170;
-	// 	}
-
-	// 	if (ws == 2) {
-	// 		waterspell.x = 394;
-	// 		waterspell.y = 251;
-	// 	}
-
-	// 	if (ws == 3) {
-	// 		waterspell.x = 354;
-	// 		waterspell.y = 271;
-	// 	}
-
-	// 	if (ws == 4) {
-	// 		waterspell.x = 95;
-	// 		waterspell.y = 252;
-	// 	}
-
-	// 	if (ws == 5) {
-	// 		waterspell.x = 215;
-	// 		waterspell.y = 231;
-	// 	}
-
-	// 	if (waterspell.isVisible == true) {
-	// 		context.drawImage(WaterSpell, waterspell.x, waterspell.y);
-	// 	}
-
-	// 	//Check for collisions between the player and collectable. Also check if the collectable is visible
-	// 	if (waterspell.isVisible & collides(player, waterspell)) {
-	// 		collectableSound.play();
-	// 		//If a collision occurs and the enemy is visible, decrease the score
-	// 		spells[2]++;
-	// 		//Change the enemy's visibility to false so that it only get picked up once
-	// 		waterspell.isVisible = false;
-	// 	}
-	// }
+	waterspells.forEach(waterspell => {
+		if (waterspell.isVisible)
+			context.drawImage(WaterSpell, waterspell.x, waterspell.y);
+	});
 	// //Draw Fire Spell
-	// for (var fs = 0; fs < 6; fs++) {
-	// 	var firespell = firespells[fs];
-
-	// 	if (fs == 1) {
-	// 		firespell.x = 114;
-	// 		firespell.y = 310;
-	// 	}
-
-	// 	if (fs == 2) {
-	// 		firespell.x = 254;
-	// 		firespell.y = 131;
-	// 	}
-
-	// 	if (fs == 3) {
-	// 		firespell.x = 514;
-	// 		firespell.y = 191;
-	// 	}
-
-	// 	if (fs == 4) {
-	// 		firespell.x = 454;
-	// 		firespell.y = 51;
-	// 	}
-
-	// 	if (fs == 5) {
-	// 		firespell.x = 334;
-	// 		firespell.y = 191;
-	// 	}
-
-	// 	if (firespell.isVisible == true) {
-	// 		context.drawImage(FireSpell, firespell.x, firespell.y);
-	// 	}
-
-	// 	//Check for collisions between the player and collectable. Also check if the collectable is visible
-	// 	if (firespell.isVisible & collides(player, firespell)) {
-	// 		collectableSound.play();
-	// 		//If a collision occurs and the enemy is visible, decrease the score
-	// 		spells[1]++;
-	// 		//Change the enemy's visibility to false so that it only get picked up once
-	// 		firespell.isVisible = false;
-	// 	}
-	// }
+	firespells.forEach(firespell => {
+		if (firespell.isVisible)
+			context.drawImage(FireSpell, firespell.x, firespell.y);
+	});
 	// //Draw Teleport Spell
-	// for (var ts = 0; ts < 6; ts++) {
-	// 	var telespell = telespells[ts];
-
-	// 	if (ts == 1) {
-	// 		telespell.x = 314;
-	// 		telespell.y = 151;
-	// 	}
-
-	// 	if (ts == 2) {
-	// 		telespell.x = 513;
-	// 		telespell.y = 32;
-	// 	}
-
-	// 	if (ts == 3) {
-	// 		telespell.x = 454;
-	// 		telespell.y = 251;
-	// 	}
-
-	// 	if (ts == 4) {
-	// 		telespell.x = 114;
-	// 		telespell.y = 271;
-	// 	}
-
-	// 	if (ts == 5) {
-	// 		telespell.x = 134;
-	// 		telespell.y = 171;
-	// 	}
-
-	// 	if (telespell.isVisible == true) {
-	// 		context.drawImage(TeleSpell, telespell.x, telespell.y);
-	// 	}
-
-	// 	//Check for collisions between the player and collectable. Also check if the collectable is visible
-	// 	if (telespell.isVisible & collides(player, telespell)) {
-	// 		collectableSound.play();
-	// 		//If a collision occurs and the enemy is visible, decrease the score
-	// 		spells[0]++;
-	// 		//Change the enemy's visibility to false so that it only get picked up once
-	// 		telespell.isVisible = false;
-	// 	}
-	// }
-	context.drawImage(cursorImage, cursor.x, cursor.y);
-	context.fillStyle = '#787878';
-	context.fillRect(0, 0, 20, 150);
+	telespells.forEach(telespell => {
+		if (telespell.isVisible)
+			context.drawImage(TeleSpell, telespell.x, telespell.y);
+	});
 
 	//Score text font and color
 	context.font = '25px Rubik Maze';
@@ -525,9 +437,6 @@ function moveCursor(event) {
 	cursor.y = event.pageY - canvas.offsetTop;
 	draw();
 }
-
-//Call the init function as soon as the page has finished loading
-window.onload = init;
 
 //Check if object a and object b are colliding
 function collides(a, b) {
