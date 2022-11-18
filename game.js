42bb02d500bf33beec2ca8d8cc59f54eaaabf9ed
CodeNate02 <n8opot8oW@gmail.com>
2022-11-18 10:36:16 -0500

    Rewrites and reorganizes game.js to be more organized, commented

--- game.js
+++ game.js
@@ -2,80 +2,6 @@ import * as Images from './assets/images';
 import * as Sounds from './assets/sounds';
 import { getAMaze } from './mazes';
 import { Scores, formatTime } from './scoreboard';
-// Set Up Variables
-var canvas, context;
-var gameRunning = false;
-var maze = getAMaze();
-const GameSounds = {
-	victoryMusic: new Audio(Sounds.WhoLikesToParty),
-	gameMusic: new Audio(Sounds.TheComplex),
-	pickUp: new Audio(Sounds.Click),
-	fire: new Audio(Sounds.Fire),
-	water: new Audio(Sounds.Water),
-	tp: new Audio(Sounds.Magic),
-	loss: new Audio(Sounds.LossBeep),
-};
-const Traps = {
-	lossScreens: {
-		fire: new Images.Resource(Images.Fire_Loss_Screen),
-		spike: new Images.Resource(Images.Spike_Loss_Screen),
-		pit: new Images.Resource(Images.Pitfall_Loss_Screen),
-	},
-	weakness: {
-		pit: 'teleport',
-		spike: 'fire',
-		fire: 'water',
-	},
-	sound: {
-		pit: GameSounds.tp,
-		spike: GameSounds.fire,
-		fire: GameSounds.water,
-	},
-};
-const WIDTH = 600;
-const HEIGHT = 400;
-const playerMoved = new Event('playerMoved'),
-	gameClick = new Event('gameClick');
-const GameEvents = {
-	events: new EventTarget(),
-	clear: function () {
-		this.events = new EventTarget();
-	},
-};
-
-window.addEventListener('mousedown', () =>
-	GameEvents.events.dispatchEvent(gameClick)
-);
-//Load image assets
-const splashScreenImage = new Images.Resource(Images.SplashPage),
-	cursorImage = new Images.Resource(Images.Cursor),
-	playerImage = new Images.Resource(Images.Player),
-	FireTrap = new Images.Resource(Images.FireTrap),
-	SpikeTrap = new Images.Resource(Images.SpikeTrap),
-	PitFall = new Images.Resource(Images.Pitfall),
-	backgroundImage = new Images.Resource(Images.Background),
-	FireSpell = new Images.Resource(Images.Fireball),
-	TeleSpell = new Images.Resource(Images.Teleport),
-	WaterSpell = new Images.Resource(Images.Water),
-	Victory = new Images.Resource(Images.Ladder),
-	VictoryImage = new Images.Resource(Images.Victory_Screen);
-
-const Timer = {
-	time: 0,
-	runningTimer: undefined,
-	start: function () {
-		this.runningTimer = setInterval(() => {
-			this.time++;
-			draw();
-		}, 1000);
-	},
-	reset: function () {
-		this.time = 0;
-	},
-	stop: function () {
-		clearInterval(this.runningTimer);
-	},
-};
 //Sprite information
 class Sprite {
 	isVisible = true;
@@ -88,7 +14,7 @@ class Sprite {
 		this.y = loc.y;
 	}
 }
-class Player extends Sprite {
+class PlayerSprite extends Sprite {
 	movementStates = {
 		up: undefined,
 		down: undefined,
@@ -96,15 +22,15 @@ class Player extends Sprite {
 		right: undefined,
 	};
 	reset() {
-		this.x = maze.start.x;
-		this.y = maze.start.y;
+		this.x = Maze.current.start.x;
+		this.y = Maze.current.start.y;
 	}
 	constructor() {
 		const startMovement = (direction, f) => {
 			if (!this.movementStates[direction])
 				this.movementStates[direction] = setInterval(f, 16);
 		};
-		super(maze.start);
+		super(Maze.current.start);
 		window.addEventListener('keydown', evt => {
 			if (gameRunning)
 				switch (evt.code) {
@@ -158,19 +84,19 @@ class TrapSprite extends Sprite {
 		super(loc);
 		this.type = type;
 		GameEvents.events.addEventListener('gameClick', () => {
-			if (this.isVisible && collides(this, cursor)) {
+			if (this.isVisible && collides(this, Cursor)) {
 				this.spell();
 			}
 		});
 		GameEvents.events.addEventListener('playerMoved', () => {
-			if (this.isVisible && collides(this, player)) {
+			if (this.isVisible && collides(this, Player)) {
 				endScreen(Traps.lossScreens[this.type], GameSounds.loss);
 			}
 		});
 	}
 	spell() {
-		if (spells[Traps.weakness[this.type]] > 0) {
-			spells[Traps.weakness[this.type]] -= 1;
+		if (Spells.count[Traps.weakness[this.type]] > 0) {
+			Spells.count[Traps.weakness[this.type]] -= 1;
 			this.isVisible = false;
 			Traps.sound[this.type].play();
 			draw();
@@ -184,50 +110,141 @@ class SpellSprite extends Sprite {
 		super(loc);
 		this.type = type;
 		GameEvents.events.addEventListener('playerMoved', () => {
-			if (this.isVisible && collides(this, player)) {
+			if (this.isVisible && collides(this, Player)) {
 				GameSounds.pickUp.play();
-				spells[this.type] += 1;
+				Spells.count[this.type] += 1;
 				this.isVisible = false;
 			}
 		});
 	}
 }
-class Goal extends Sprite {
+class GoalSprite extends Sprite {
 	constructor() {
-		super(maze.victory);
+		super(Maze.current.victory);
 		GameEvents.events.addEventListener('playerMoved', () => {
-			if (this.isVisible && collides(this, player)) {
+			if (this.isVisible && collides(this, Player)) {
 				endScreen(VictoryImage, GameSounds.victoryMusic);
 				gameWin();
 			}
 		});
 	}
+	reset() {
+		this.x = Maze.current.victory.x;
+		this.y = Maze.current.victory.y;
+	}
 }
+// Set Up Variables
+const WIDTH = 600,
+	HEIGHT = 400,
+	canvas = document.getElementById('myCanvas'),
+	context = canvas.getContext('2d', { willReadFrequently: true }),
+	//Various sounds
+	GameSounds = {
+		victoryMusic: new Audio(Sounds.WhoLikesToParty),
+		gameMusic: new Audio(Sounds.TheComplex),
+		pickUp: new Audio(Sounds.Click),
+		fire: new Audio(Sounds.Fire),
+		water: new Audio(Sounds.Water),
+		tp: new Audio(Sounds.Magic),
+		loss: new Audio(Sounds.LossBeep),
+	},
+	//Traps contains all values pertaining to traps including loss screens, weaknesses, sound effects, and currently loaded sprites
+	Traps = {
+		lossScreens: {
+			fire: new Images.Resource(Images.Fire_Loss_Screen),
+			spike: new Images.Resource(Images.Spike_Loss_Screen),
+			pit: new Images.Resource(Images.Pitfall_Loss_Screen),
+		},
+		weakness: {
+			pit: 'tp',
+			spike: 'fire',
+			fire: 'water',
+		},
+		sound: {
+			pit: GameSounds.tp,
+			spike: GameSounds.fire,
+			fire: GameSounds.water,
+		},
+		images: {
+			fire: new Images.Resource(Images.FireTrap),
+			spike: new Images.Resource(Images.SpikeTrap),
+			pit: new Images.Resource(Images.Pitfall),
+		},
+		sprites: {},
+	},
+	//Spells contains a Spell image resources, count of player's available spell, and currently loaded Spell sprites
+	Spells = {
+		count: {},
+		images: {
+			fire: new Images.Resource(Images.Fireball),
+			tp: new Images.Resource(Images.Teleport),
+			water: new Images.Resource(Images.Water),
+		},
+		sprites: {},
+	},
+	//Game Events tracks varous events, is reset on new-game
+	GameEvents = {
+		events: new EventTarget(),
+		clear: function () {
+			this.events = new EventTarget();
+		},
+	},
+	PlayerMoved = new Event('playerMoved'),
+	GameClick = new Event('gameClick'),
+	//Timer counts up time taken to complete game
+	Timer = {
+		time: 0,
+		runningTimer: undefined,
+		start: function () {
+			this.runningTimer = setInterval(() => {
+				this.time++;
+				draw();
+			}, 1000);
+		},
+		reset: function () {
+			this.time = 0;
+		},
+		stop: function () {
+			clearInterval(this.runningTimer);
+		},
+	},
+	//Various images
+	splashScreenImage = new Images.Resource(Images.SplashPage),
+	cursorImage = new Images.Resource(Images.Cursor),
+	playerImage = new Images.Resource(Images.Player),
+	backgroundImage = new Images.Resource(Images.Background),
+	Victory = new Images.Resource(Images.Ladder),
+	VictoryImage = new Images.Resource(Images.Victory_Screen),
+	//Maze contains currently retrieved values of maze
+	Maze = {
+		current: getAMaze(),
+		new: function () {
+			this.maze = getAMaze();
+		},
+	},
+	//Player sprite tracks and moves player
+	Player = new PlayerSprite(),
+	//Goal sprite tracks current position of the maze's end goal
+	Goal = new GoalSprite(),
+	// Cursor settings
+	Cursor = {
+		x: 0,
+		y: 0,
+		width: 11,
+		height: 11,
+		isVisible: true,
+	};
 
-var cursor = {
-	x: 0,
-	y: 0,
-	width: 11,
-	height: 11,
-	isVisible: true,
-};
+var gameRunning = false;
 
-var spells;
-
-var player = new Player();
-// Cursor settings
-
-var victory = new Goal(),
-	spiketraps = [],
-	firetraps = [],
-	pitfalls = [],
-	firespells = [],
-	telespells = [],
-	waterspells = [];
+window.addEventListener('mousedown', () =>
+	GameEvents.events.dispatchEvent(GameClick)
+);
+window.addEventListener('mousedown', () =>
+	console.log(`${Cursor.x},${Cursor.y}`)
+);
 
 function init() {
-	canvas = document.getElementById('myCanvas');
-	context = canvas.getContext('2d', { willReadFrequently: true });
 	//Draw splash screen
 	context.drawImage(splashScreenImage, 0, 0);
 	//Listen for player click on splash screen
@@ -239,51 +256,38 @@ function init() {
 export default init;
 // Set Up Functions
 function game() {
-	player.reset();
+	// Reset player, timer, victory location, spell counts
+	Player.reset();
 	Timer.reset();
-	spells = {
-		teleport: 1,
+	Goal.reset();
+	Spells.count = {
+		tp: 1,
 		fire: 0,
 		water: 0,
 	};
-	GameSounds.gameMusic.play();
-	//Generate Fire Traps
 
-	firetraps = [];
-	for (let f = 0; f < maze.traps.fire.length; f++) {
-		firetraps.push(new TrapSprite(maze.traps.fire[f], 'fire'));
+	//Generate maze contents
+	for (let key of Object.keys(Maze.current.traps)) {
+		Traps.sprites[key] = [];
+		for (let trap of Maze.current.traps[key]) {
+			Traps.sprites[key].push(new TrapSprite(trap, key));
+		}
 	}
-	spiketraps = [];
-	// //Generate Spike Traps
-	for (let f = 0; f < maze.traps.spike.length; f++) {
-		spiketraps.push(new TrapSprite(maze.traps.spike[f], 'spike'));
+	for (let key of Object.keys(Maze.current.spells)) {
+		Spells.sprites[key] = [];
+		for (let spell of Maze.current.spells[key]) {
+			Spells.sprites[key].push(new SpellSprite(spell, key));
+		}
 	}
-	pitfalls = [];
-	// //Generate pitfalls
-	for (let f = 0; f < maze.traps.pit.length; f++) {
-		pitfalls.push(new TrapSprite(maze.traps.pit[f], 'pit'));
-	}
-	firespells = [];
-	// //Generate fire spells
-	for (let f = 0; f < maze.spells.fire.length; f++) {
-		firespells.push(new SpellSprite(maze.spells.fire[f], 'fire'));
-	}
-	telespells = [];
-	// //Generate teleport spells
-	for (let f = 0; f < maze.spells.tp.length; f++) {
-		telespells.push(new SpellSprite(maze.spells.tp[f], 'teleport'));
-	}
-	waterspells = [];
-	// //Generate water spells
-	// for (let ws = 0; ws < 6; ws++) {
-	for (let f = 0; f < maze.spells.water.length; f++) {
-		waterspells.push(new SpellSprite(maze.spells.water[f], 'water'));
-	}
+
+	//Track mouse movements
 	canvas.onmousemove = moveCursor;
-	gameRunning = true;
+	//Start music, timer
+	GameSounds.gameMusic.play();
 	Timer.start();
+
+	gameRunning = true;
 	draw();
-	//Track cursor movements
 }
 function draw() {
 	if (!gameRunning) {
@@ -293,62 +297,44 @@ function draw() {
 	//Potatoes (why did I comment 'Potatoes' here?  This was a sophomore-year me comment.  Present me has no idea, but I can't bring myself to delete it.  Long live the Potatoes.)
 	clear();
 	context.drawImage(backgroundImage, 0, 0);
-	context.drawImage(maze.screen,95,30);
+	context.drawImage(Maze.current.screen, 95, 30);
 	//Draw player
-	context.drawImage(playerImage, player.x, player.y);
-	context.drawImage(cursorImage, cursor.x, cursor.y);
+	context.drawImage(playerImage, Player.x, Player.y);
+	context.drawImage(cursorImage, Cursor.x, Cursor.y);
 	//Draw Victory Pickup
-	context.drawImage(Victory, victory.x, victory.y);
-
-	//Draw Fireball
-	pitfalls.forEach(pitfall => {
-		if (pitfall.isVisible) context.drawImage(PitFall, pitfall.x, pitfall.y);
-	});
-	//Draw Spike Trap
-	spiketraps.forEach(spiketrap => {
-		if (spiketrap.isVisible)
-			context.drawImage(SpikeTrap, spiketrap.x, spiketrap.y);
-	});
-	//Draw Fireball
-	firetraps.forEach(firetrap => {
-		if (firetrap.isVisible)
-			context.drawImage(FireTrap, firetrap.x, firetrap.y);
-	});
-	// //Draw Water Spell
-	waterspells.forEach(waterspell => {
-		if (waterspell.isVisible)
-			context.drawImage(WaterSpell, waterspell.x, waterspell.y);
-	});
-	// //Draw Fire Spell
-	firespells.forEach(firespell => {
-		if (firespell.isVisible)
-			context.drawImage(FireSpell, firespell.x, firespell.y);
-	});
-	// //Draw Teleport Spell
-	telespells.forEach(telespell => {
-		if (telespell.isVisible)
-			context.drawImage(TeleSpell, telespell.x, telespell.y);
-	});
-
+	context.drawImage(Victory, Goal.x, Goal.y);
+	for (let key of Object.keys(Traps.sprites)) {
+		for (let trap of Traps.sprites[key]) {
+			if (trap.isVisible)
+				context.drawImage(Traps.images[key], trap.x, trap.y);
+		}
+	}
+	for (let key of Object.keys(Spells.sprites)) {
+		for (let spell of Spells.sprites[key]) {
+			if (spell.isVisible)
+				context.drawImage(Spells.images[key], spell.x, spell.y);
+		}
+	}
 	//Score text font and color
-	context.font = '25px Rubik Maze';
-	context.textAlign = "center";
-	context.fillStyle = '#000000';
+	[context.font, context.textAlign, context.fillStyle] = [
+		'25px Rubik Maze',
+		'center',
+		'#000000',
+	];
 	//Display score
-	context.fillText(spells.teleport, 52, 37);
-	context.fillText(spells.fire, 52, 73);
-	context.fillText(spells.water, 52, 107);
-	context.font = '20px Rubik Maze';
-	context.textAlign = "right";
+	context.fillText(Spells.count.tp, 52, 37);
+	context.fillText(Spells.count.fire, 52, 73);
+	context.fillText(Spells.count.water, 52, 107);
+	[context.font, context.textAlign] = ['20px Rubik Maze', 'right'];
 	context.fillText(formatTime(Timer.time), 69, 140);
 } //End update function
 function clear() {
-	context.clearRect(0, 0, WIDTH, HEIGHT);
+	context.clearRect(0, 0, WIDTH, HEIGHT); //Erases all items from the context
 }
-
+//Move and redraw the cursor image whenever the mouse is moved
 function moveCursor(event) {
-	cursor.x = event.pageX - canvas.offsetLeft;
-	cursor.y = event.pageY - canvas.offsetTop;
+	Cursor.x = event.pageX - canvas.offsetLeft;
+	Cursor.y = event.pageY - canvas.offsetTop;
 	draw();
 }
 //Check if object a and object b are colliding
@@ -360,61 +346,64 @@ function collides(a, b) {
 		a.y + a.height > b.y
 	);
 }
+//Moves player horizontally and checks if maze wall is there
 function movePlayerX(move) {
-	let newX = player.x + move;
+	let newX = Player.x + move;
 	if (
 		(move > 0 &&
 			!context
 				.getImageData(
-					newX + player.width - 1,
-					player.y,
+					newX + Player.width - 1,
+					Player.y,
 					1,
-					player.height
+					Player.height
 				) //Check to the right of the player when moving right
 				.data.includes(72)) ||
 		(move < 0 &&
 			!context
-				.getImageData(newX, player.y, 1, player.height) //Check to the left of the player when moving left
+				.getImageData(newX, Player.y, 1, Player.height) //Check to the left of the player when moving left
 				.data.includes(72))
 	) {
-		player.x = newX;
-		GameEvents.events.dispatchEvent(playerMoved);
+		Player.x = newX;
+		GameEvents.events.dispatchEvent(PlayerMoved);
 		draw();
 	}
 }
+//Moves player vertically and checks if a maze wall is there
 function movePlayerY(move) {
-	let newY = player.y - move;
+	let newY = Player.y - move;
 	if (
 		(move > 0 &&
 			!context
-				.getImageData(player.x, newY, player.width, 1)
+				.getImageData(Player.x, newY, Player.width, 1)
 				.data.includes(72)) ||
 		(move < 0 &&
 			!context
 				.getImageData(
-					player.x,
-					newY + player.height - 1,
-					player.width,
+					Player.x,
+					newY + Player.height - 1,
+					Player.width,
 					1
 				)
 				.data.includes(72))
 	) {
-		player.y = newY;
-		GameEvents.events.dispatchEvent(playerMoved);
+		Player.y = newY;
+		GameEvents.events.dispatchEvent(PlayerMoved);
 		draw();
 	}
 }
+/*Draw a game end screen (Game Over or Victory)
+Run various actions that need to be done between games*/
 function endScreen(screen, sound) {
+	gameRunning = false;
 	GameEvents.clear();
-	Timer.stop();
 	GameSounds.gameMusic.pause();
-	(GameSounds.gameMusic = new Audio(Sounds.TheComplex)),
-		(gameRunning = false);
-	context.drawImage(screen, 0, 0);
+	GameSounds.gameMusic = new Audio(Sounds.TheComplex);
 	sound.play();
+	Timer.stop();
 	clear();
 	context.drawImage(screen, 0, 0);
-	//Listen for player click on splash screen
+	//Listen for player click on splash screen and restart the game on click
 	canvas.onmousedown = () => {
 		canvas.onmousedown = undefined;
 		sound.pause();
@@ -422,7 +411,8 @@ function endScreen(screen, sound) {
 	};
 }
 function gameWin() {
+	//Run actions specific to post-victory game runs
 	console.log(`Game won in ${formatTime(Timer.time)}`);
-	Scores.addTime(Timer.time, maze.mazeID);
-	maze = getAMaze();
+	Scores.addTime(Timer.time, Maze.current.mazeID);
+	Maze.new();
 }
